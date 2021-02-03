3d672032db03cb7bf754be732c38ce5bbb45185f
Hugo Robas <robashugo@gmail.com>
2021-02-03 15:14:02 -0800

    2/2/2021

--- Lesson_2/test2.js
+++ Lesson_2/test2.js
@@ -8,9 +8,9 @@ function invalidNumber(number) {
   return number.trimStart() === '' || Number.isNaN(Number(number));
 }
 
-prompt('Welcome to the car loan Calculator!');
+console.log('Welcome to the car loan Calculator!');
 
-prompt("What's your loan amount?");
+prompt("What's your loan amount? Please provide a number");
 let loanAmount = readline.question();
 
 while (invalidNumber(loanAmount)) {
@@ -27,14 +27,14 @@ while (invalidNumber(annualPerRate)) {
 }
 
 
-prompt("please provide your loan duration in months (eg 5.5 for a 5 years and a half)");
+prompt("please provide your loan duration in years (eg 5.5 for a 5 years and a half)");
 let loanLength = readline.question();
 
+const LOAN_MONTH = loanLength * 12;
 
-//const LOAN_MONTH = loanLength * (12 * 1);
-const MONTHLY_INTEREST_RATE = (annualPerRate / 100) / loanLength; 
+const MONTHLY_INTEREST_RATE = annualPerRate / 12 / 100;
 
-let monthlyPayment = Number(loanAmount * (MONTHLY_INTEREST_RATE / (1 - Math.pow((1 + MONTHLY_INTEREST_RATE),(- loanLength)))));
+let monthlyPayment = loanAmount * (MONTHLY_INTEREST_RATE / (1 - Math.pow(1 + MONTHLY_INTEREST_RATE, - LOAN_MONTH)));
 monthlyPayment = monthlyPayment.toFixed(2);
 
 console.log(`Your monthly payment is of $${monthlyPayment}`);
