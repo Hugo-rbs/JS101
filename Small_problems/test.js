4c36964517469608af39d2240808b4756c965869
Hugo Robas <robashugo@gmail.com>
2021-04-19 22:08:47 -0700

    4/19/21

--- Small_problems/test.js
+++ Small_problems/test.js
@@ -1,30 +1,23 @@
-/*
+// let arr = [2, 5, 13];
+// let newArr = []
+// for (let idx = 0; idx < arr.length; idx++) {
 
-input: string
-output: the same string with non-alphanumeric elements replaced by spaces - no consecutive spaces 
+// }
 
-1. take the string check if it contains any non-alphnumeric elements
-2. replace the elements by spaces 
-3.Remove consecutive spaces 
-4. return result 
 
-ex :
-cleanUp("---what's my +*& line?");    // " what s my line "
-
-*/
-
-function isLetter(char) {
-  return char >= 'a' && char <= 'z';
-}
-
-function cleanUp(string) {
-  let result = '';
-  for (let idx = 0; idx  <= string.length; idx++) {
-    if (isLetter(string[idx])) {
-      result += string[idx];
-    }
+function runningTotal(arr) {
+  let result = [];
+  if (arr.length < 1) {
+    return result;
   }
-  return result;
+  result = [arr.shift()];
+  for (let idx = 0; idx < arr.length; idx++) {
+    result.push(result[idx] + arr[idx]);
+  }
+  console.log(result);
 }
 
-console.log(cleanUp("---what's my +*& line?"))
+runningTotal([2, 5, 13]);
+runningTotal([14, 11, 7, 15, 20]);
+runningTotal([3]);
+runningTotal([]);
--- Small_problems/test2.js
+++ Small_problems/test2.js
@@ -1,29 +1,5 @@
-function cleanUp(text) {
-  let cleanText = '';
-
-  for (let idx = 0; idx < text.length; idx += 1) {
-    if (isLowerCaseLetter(text[idx]) || isUpperCaseLetter(text[idx])) {
-      cleanText += text[idx];
-    } else if (cleanText[cleanText.length -1] !== ' ') {
-      cleanText += ' ';
-    }
-  }
-
-  return cleanText;
-}
-
-function isLowerCaseLetter(char) {
-  return char >= 'a' && char <= 'z';
-}
-
-function isUpperCaseLetter(char) {
-  return char >= 'A' && char <= 'Z';
-}
-
-console.log(cleanUp("---what's my +*& line?"));
-
-/*
-else if (cleanText[cleanText.length - 1] !== ' ') {
-  cleanText += ' ';
-}
-*/
+let string = 'Captain Javascript';
+// console.log(string.split(' ')[0] + ' Ruby');
+// console.log(string.substring(0, 8) + 'Ruby');
+string = string.replace('Javascript', 'Ruby');
+console.log(string);
