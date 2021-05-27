62aefc38012d96d94baa5c833acc47db22223cd8
Hugo Robas <robashugo@gmail.com>
2021-05-27 08:57:48 -0700

    5/27/2021

--- Small_problems/test.js
+++ Small_problems/test.js
@@ -1,7 +1,17 @@
-function sumEvenNumberRow(rowNumber) {
-
+function isLetter(char) {
+  return char.toLowerCase() >= 'a' && char.toLowerCase() <= 'z';
 }
 
-function createRow(startInteger, rowLength) {
-
+function cleanUp(string) {
+  let result = '';
+  for (let idx = 0; idx <= string.length; idx += 1) {
+    if (isLetter(string[idx])) {
+      result += string[idx];
+    } if (result.length -1 !== ' ') {
+      result += ' ';
+    }
+  }
+  return result;
 }
+
+console.log(cleanUp("---what's my +*& line?"));
