6700673c75131d4188cbffc0e4ebe78dee54384f
Hugo Robas <robashugo@gmail.com>
2021-07-12 10:53:10 -0700

    7/12/21

--- Lesson_5/test.js
+++ Lesson_5/test.js
@@ -1,1 +1,17 @@
-let str1 = 'blueberry';
+let vehicle = {
+  Manufacturer : 'Tesla',
+  Model : 'Model X',
+  year : 2015,
+  range : 295,
+  Seats : 7
+};
+
+let keys = Object.keys(vehicle);
+let keys2 = [];
+
+for (let prop in vehicle) {
+  keys2.push(prop);
+}
+
+console.log(keys);
+console.log(keys2);
